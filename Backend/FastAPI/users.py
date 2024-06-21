e734ac820394e9fda01f05175ee49a4c5e67e547
Erikajpb <erikajpinedab@gmail.com>
2024-06-21 11:43:22 -0500

    adding the post, put and delete functions

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -50,11 +50,49 @@ async def user(id: int):
 async def user(id: int) :
     return search_user(id)
 
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "User already exists"}
+ 
+    users_list.append(user)
+    return user
+
+
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user 
+            found = True
+    
+    if not found:
+        return {"error": "User not found"}
+   
+    return user
+
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+            
+    if not found:
+        return {"error": "User not found"}
+    
+    return {"message": "User deleted successfully"}
+
+    
+
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
         return {"error": "User not found" }
-    
-
