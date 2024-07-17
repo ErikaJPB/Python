e4b1106c7632ac521bc29fe202097c8c0776b424
Erikajpb <erikajpinedab@gmail.com>
2024-07-17 15:21:06 -0500

    final changes

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,16 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+from os import getenv
 
-db_client = MongoClient().local
+load_dotenv()
 
+mongodb_uri = getenv("MONGODB_URI")
+
+if not mongodb_uri:
+    raise ValueError("No MONGODB_URI found in environment variables")
+
+# Local database
+#db_client = MongoClient().local
+
+# Remote database
+db_client = MongoClient(mongodb_uri).test
